b1773425f40ba9c59b3aafc2de82506105b06ed7
Mungusaur <pembertonmungus@gmail.com>
2026-08-14 12:53:50 +1200

    Added neccessary conversions for the encryption system.

--- code_to_send_marker/main.py
+++ code_to_send_marker/main.py
@@ -5,34 +5,71 @@ import subprocess
 import sys
 import os
 
+user = "default"
 
 def check_key():
     dir_path = Path(__file__).resolve().parent
-    a = dir_path + r"\donotopen.key"
+    a = str(dir_path) + r"\donotopen.key"
     with open(a, "r") as file:
         b = file.read()
-    if a == "":
+    if b == "":
         key = Fernet.generate_key()
+        key = str(key)
         with open(a, "w") as file:
             file.write(key)
-    else:
-        fernet = Fernet(key)
-        return fernet
+    with open(a, "r") as file:
+        key = file.read()
+    key = key[2:-1]
+    key = key.encode('utf-8')
+    return key
 
 def dict_to_txt(txt, dictionary):
-    with open(txt, "w") as file:
+    dir_path = Path(__file__).resolve().parent
+    near_tru_path = str(dir_path) + "/"
+    tru_path = near_tru_path + txt
+    with open(tru_path, "w") as file:
         file.write(dictionary)
 
 def txt_to_dict(txt, dictionary):
-    with open(txt, "r") as file:
+    dir_path = Path(__file__).resolve().parent
+    near_tru_path = str(dir_path) + "/"
+    tru_path = near_tru_path + txt
+    with open(tru_path, "r") as file:
         a = file.read()
     b = dict(a)
     dictionary = b
     return dictionary
 
-def encrypt(txt):
-    with open(txt, "r") as file:
+def __encrypt__(txt):
+    dir_path = Path(__file__).resolve().parent
+    near_tru_path = str(dir_path) + "/"
+    tru_path = near_tru_path + txt
+    with open(tru_path, "r") as file:
         txt_content = file.read()
-    key = check_key
+    key = check_key()
     fernet = Fernet(key)
-    #Continue this function
+    txt_content = txt_content.encode("utf-8")
+    new_data = fernet.encrypt(txt_content)
+    new_data = str(new_data)
+    new_data = new_data[2:-1]
+    with open(tru_path, "w") as file:
+        file.write(new_data)
+    return "complete"
+
+def __decrypt__(txt):
+    dir_path = Path(__file__).resolve().parent
+    near_tru_path = str(dir_path) + "/"
+    tru_path = near_tru_path + txt
+    with open(tru_path, "r") as file:
+        txt_content = file.read()
+    key = check_key()
+    fernet = Fernet(key)
+    new_data = fernet.decrypt(txt_content)
+    new_data = str(new_data)
+    new_data = new_data[3:-1]
+    with open(tru_path, "w") as file:
+        file.write(new_data)
+
+def encrypt_user(txt, user):
+    pass
+
